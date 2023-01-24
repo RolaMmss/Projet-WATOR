e575ccb15b25befd5a41bde4a7173f8edda50e66
RolaMmss <rola.el-moallem@hotmail.fr>
2023-01-24 16:22:14 +0100

    pseudo modified

--- pseudo_code.py
+++ pseudo_code.py
@@ -7,7 +7,9 @@ class World:
         pass
     
 
-class Animal:
+class Fish:
+    def possible_moves():
+        pass
     def move():
         up : y = y+1
         down : y = y-1
@@ -16,38 +18,23 @@ class Animal:
         freeze : x == x and y == y 
     def reproduce():
         reproduction_time : int
-        pass
-    def die():
-        pass
 
-class Shark(Animal):
+
+
+class Shark(Fish):
     nb_sharks : int
     energy : int
     shark_reproduction_time : int
-    shark_position (x,y)
-    def search_fish():
-        pass
-        else move()
-    def eat_fish():
+    
+    def possible_moves():
         pass
     
-
-class Fish(Animal):
-    nb_fish : int
-    fish_reproduction_time : int
-    fish_position (x,y)
-    def search_empty_case():
+    def move():
         pass
-
-
-def main():
-    display_world()
-    add_animals()
-    turn()
+    
+    def reproduce():
+        reproduction_time : int
     
 
-    turn = 0
 
-
-    main()
 
