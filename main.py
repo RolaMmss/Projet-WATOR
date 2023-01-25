2e7f3b01807f7f33ed4e0fa4728ca086ad969706
Thomas59 <thomas.lemay59@outlook.fr>
2023-01-25 13:55:35 +0100

    ajout de la fonction move dans fish

--- main.py
+++ main.py
@@ -2,36 +2,43 @@ from fish import Fish
 from shark import Shark
 from world import World
 import random as rd
+import time
+import os
 
 ####################################
 #        Initialization            #
 ####################################
 
-number_of_fish = 50         #int(input("Enter the number of fish : "))
+number_of_fish = 30        #int(input("Enter the number of fish : "))
 number_of_sharks = 20       #int(input("Enter the number of sharks : "))
 
 #creation of the world
 day = 1
-world = World(20, 20)
+rows = 20
+cols = 20
+world = World(rows, cols)
 
-for i in range(10):
-    shark = Shark(rd.randint(1,19), rd.randint(1,19))
+for i in range(number_of_sharks):
+    shark = Shark(rd.randint(1,rows - 1), rd.randint(1,cols - 1))
     world.add_shark(shark)
 
-fish1 = Fish(10, 10)
-world.add_fish(fish1)
+for i in range(number_of_fish):
+    fish = Fish(rd.randint(1, rows - 1), rd.randint(1, cols - 1))
+    world.add_fish(fish)
 
 #####################################
 #              START                #
 #####################################
-while day < 2:
-    print("jour : " + str(day))
+while day < 6:
+    print("days : " + str(day))
     world.display_grid()
     for shark in world.sharks:
         shark.move()
-        shark.hunt()
         shark.reproduce()
     for fish in world.fishes:
-        fish.move()
+        fish.move(world)
         fish.reproduce()
+
     day += 1
+    
+    time.sleep(2)
