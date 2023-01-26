a85cb8cdd819c766a289a3900056b9adc5c620fd
ForskyOnly <foulseumsky@gmail.com>
2023-01-26 09:35:00 +0100

    correction fonction + ajout commentaires

--- shark.py
+++ shark.py
@@ -16,27 +16,19 @@ class Shark(Fish):
         # if day % self.reproduction_number  == 0 : #and isinstance(self[self.x][self.y], Fish):
         #     new_shark = Shark()
         #     world.sharks.append(new_shark)
-        #     world.table[self.y][self.x] = '🦈'   
-                     
+        #     world.table[self.y][self.x] = '🦈'
     def hunt(self):
         pass
     
     def shark_dead(self, world):
-            # Si la case actuelle [x,y] a un objet Fish et si l'energie du requi est < 7
-        if self[self.x][self.y] and isinstance(self[self.x][self.y], Fish) and self.energy < 7:
-            # Augmente l'énergie du requin
-            self.energy += 1
-            # Enléve -1 du nombre des fish dans l'objet "world"
-            world.fishes -= 1
-            # Supprime le poisson de la liste de poissons dans l'objet "world"
-            world.fishes.remove(self[self.x][self.y])
+            # Si la case actuelle ([x,y]) a un objet Fish et si l'energie du requin est < 7
+        if  world.table[self.x][self.y] and isinstance([self.x][self.y], Fish) and self.energy < 7:
+            self.energy += 1                            # Augmente l'énergie du requin
+            world.fishes -= 1                           # Enléve -1 du nombre des fish dans l'objet "world"
+            world.fishes.remove(self[self.x][self.y])   # Supprime le poisson de la liste de poissons dans l'objet "world"
         else:
-            # Sinon, enléve - 1 à l'énergie du requin
-            self.energy -= 1
-            # Vérifie si l'énergie du requin est inférieure ou égale à 0
-        if self.energy <= 0:
-            # Si oui, enléve -1 du nombre des requins dans l'objet "world"
-            world.sharks -= 1
-            # Supprime l'objet requin de la liste de requins dans l'objet "world"
-            world.sharks.remove(self[self.x][self.y])
+            self.energy -= 1                            # Sinon, enléve - 1 à l'énergie du requin
+        if  self.energy <= 0:                           # Vérifie si l'énergie du requin est inférieure ou égale à 0
+            world.sharks -= 1                           # Si oui, enléve -1 du nombre des requins dans l'objet "world"
+            world.sharks.remove(self[self.x][self.y])   # Supprime l'objet requin de la liste de requins dans l'objet "world"
   
