5300e6ecc28104c8eb530b3754cd1b53f2625cda
jadamin <jadaminpro@gmail.com>
2023-01-26 07:03:32 +0100

    changes

---
+++ main.py
@@ -0,0 +1,17 @@
+####################################
+#        Initialization            #
+####################################
+
+reproduction_time = 0
+shark_reproduction_time = 0
+energy = 0
+number_of_fish = 50         #int(input("Enter the number of fish : "))
+number_of_sharks = 20       #int(input("Enter the number of sharks : "))
+
+
+world = World(row, column)
+world.add_animals()
+
+#####################################
+#              START                #
+#####################################
---
+++ pseudo_code.py
@@ -0,0 +1,49 @@
+#############################
+#     Classes needed:       #
+#############################
+
+class World:
+    def display_world(row : int, col : int):
+        pass
+    def add_animals():
+        pass
+    def play_turn():
+        pass
+    
+
+class Fish:
+    def possible_moves():
+        pass
+    def move():
+        up : y = y+1
+        down : y = y-1
+        right : x = x+1
+        left : x = x-1
+        freeze : x == x and y == y 
+    def reproduce():
+        reproduction_time : int
+
+
+
+class Shark(Fish):
+    nb_sharks : int
+    energy : int
+    shark_reproduction_time : int
+    
+    def possible_moves():
+        pass
+    
+    def move():
+        pass
+    
+    def reproduce():
+        reproduction_time : int
+
+
+
+
+
+
+
+
+
